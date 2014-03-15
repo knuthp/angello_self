926c92da4a7be3c65af163cf2a8caa8a158b000c
knuthp <knuthp@gmail.com>
2014-03-15 15:40:46 +0100

    Added test for MainCtrl

---
+++ angularjs-hello/tests/MainCtrlSpec.js
@@ -0,0 +1,32 @@
+
+
+
+describe('Controller : MainCtrl', function () {
+	
+	beforeEach(module('Angello'));
+	
+	var MainCtrl, scope;
+	
+	beforeEach(inject(function ($controller) {
+		scope = {};
+		MainCtrl = $controller('MainCtrl', {
+			$scope : scope
+		});
+	}));
+	
+	
+	it('should attach a list of story types to the scope', function() {
+		expect(scope.types).toBeDefined();
+	});
+	
+	it('should attach a list of story statuses to the scope', function() {
+		expect(scope.statuses).toBeDefined();
+	});
+	
+	it('should attach a list of stories to the scope', function() {
+		expect(scope.stories).toBeDefined();
+	});
+	
+	
+});
+
